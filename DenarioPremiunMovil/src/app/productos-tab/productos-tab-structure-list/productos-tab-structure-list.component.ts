b0da6b2db0efeb51de70e990b1b4f09e6cd5e995
Luis Castillo <luis.castillo@kiberno.com>
2025-10-02 11:09:50 -0400

    cambios para mejorar

--- DenarioPremiunMovil/src/app/productos-tab/productos-tab-structure-list/productos-tab-structure-list.component.ts
+++ DenarioPremiunMovil/src/app/productos-tab/productos-tab-structure-list/productos-tab-structure-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
 import { ProductStructureUtil } from 'src/app/modelos/ProductStructureUtil';
 import { Enterprise } from 'src/app/modelos/tables/enterprise';
 import { ProductStructureCount } from 'src/app/modelos/tables/productStructureCount';
@@ -10,13 +10,13 @@ import { ReturnLogicService } from 'src/app/services/returns/return-logic.servic
 import { SynchronizationDBService } from 'src/app/services/synchronization/synchronization-db.service';
 
 @Component({
-    selector: 'productos-tab-structure-list',
-    templateUrl: './productos-tab-structure-list.component.html',
-    styleUrls: ['./productos-tab-structure-list.component.scss'],
-    standalone: false
+  selector: 'productos-tab-structure-list',
+  templateUrl: './productos-tab-structure-list.component.html',
+  styleUrls: ['./productos-tab-structure-list.component.scss'],
+  standalone: false
 })
 export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
-
+  public cdr: ChangeDetectorRef;
 
   productStructureService = inject(ProductStructureService);
   productService = inject(ProductService);
@@ -33,7 +33,7 @@ export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
   @Input()
   devolucion: Boolean = false;
   @Input()
-  pedido: Boolean= false; 
+  pedido: Boolean = false;
 
   tpsSeleccionada!: TypeProductStructure;
   typeProductStructureList: TypeProductStructure[] = [];
@@ -48,10 +48,13 @@ export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
   searchSub: any;
   validateReturnSub: any;
 
-  constructor() { }
+  constructor() {
+    this.cdr = inject(ChangeDetectorRef);
+  }
 
 
   ngOnInit() {
+
     if (this.showProductStructure) {
       this.productStructures = true;
       this.getTypeProductStructures();
@@ -71,12 +74,12 @@ export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
       this.productStructures = false;
     });
 
-    
-    if (this.pedido){
+
+    if (this.pedido) {
       //hacemos espacio para el boton de carrito
       this.buttonSize = 5;
     }
-      
+
 
   }
 
@@ -87,7 +90,7 @@ export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
   }
 
   getTypeProductStructures() {
-    this.productStructureService.getTypeProductStructuresByIdEnterprise(this.dbServ.getDatabase(),this.empresaSeleccionada.idEnterprise).then(() => {
+    this.productStructureService.getTypeProductStructuresByIdEnterprise(this.dbServ.getDatabase(), this.empresaSeleccionada.idEnterprise).then(() => {
       this.typeProductStructureList = this.productStructureService.typeProductStructureList;
       this.tpsSeleccionada = this.typeProductStructureList[0];
       this.getProductStructures();
@@ -95,13 +98,13 @@ export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
   }
 
   getProductStructures() {
-    this.productStructureService.getProductStructuresByIdTypeProductStructureAndIdEnterprise(this.dbServ.getDatabase(),this.tpsSeleccionada.idTypeProductStructure, this.empresaSeleccionada.idEnterprise).then(() => {
+    this.productStructureService.getProductStructuresByIdTypeProductStructureAndIdEnterprise(this.dbServ.getDatabase(), this.tpsSeleccionada.idTypeProductStructure, this.empresaSeleccionada.idEnterprise).then(() => {
       this.productStructureList = [];
 
-      if(this.pedido && this.orderServ.userCanSelectChannel){
+      if (this.pedido && this.orderServ.userCanSelectChannel) {
         // [userCanSelectChannel] hay que filtrar por orderType
         let structs = this.orderServ.orderTypeProductStructure.filter((s) => s.idOrderType === this.orderServ.tipoOrden.idOrderType)
-        this.productStructureList =  this.productStructureService.productStructureCountList.filter((ps) => structs.find((s) => s.coProductStructure === ps.coProductStructure));
+        this.productStructureList = this.productStructureService.productStructureCountList.filter((ps) => structs.find((s) => s.coProductStructure === ps.coProductStructure));
         /* quizas innecesario
         //filtramos los productStructures que corresponden al orderType
         let idProductStructureList: number[] = [];
@@ -109,24 +112,24 @@ export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
         const set = new Set(idProductStructureList);
         this.productStructureService.idProductStructureList = this.productStructureService.idProductStructureList.filter(id => set.has(id));
         */
-        }else{
+      } else {
         //caso regular
         this.productStructureList = this.productStructureService.productStructureCountList;
       }
-      this.productService.getFavoriteProductCount(this.dbServ.getDatabase(),this.empresaSeleccionada.idEnterprise).then(count => {
-        let favname=this.orderServ.tags.get("PED_FAVORITO");
-        this.favoriteButtonLabel = (favname != undefined) ? favname : 'Favoritos' ;
-        this.favoriteButtonLabel += ' ('+count+')';
+      this.productService.getFavoriteProductCount(this.dbServ.getDatabase(), this.empresaSeleccionada.idEnterprise).then(count => {
+        let favname = this.orderServ.tags.get("PED_FAVORITO");
+        this.favoriteButtonLabel = (favname != undefined) ? favname : 'Favoritos';
+        this.favoriteButtonLabel += ' (' + count + ')';
       })
 
-      
-      if(this.orderServ.featuredProducts){
-        this.productService.getFeaturedProductCount(this.dbServ.getDatabase(),this.empresaSeleccionada.idEnterprise).then(count => {
-          this.featuredButtonLabel = this.orderServ.nameProductLine + ' ('+count+')';
+
+      if (this.orderServ.featuredProducts) {
+        this.productService.getFeaturedProductCount(this.dbServ.getDatabase(), this.empresaSeleccionada.idEnterprise).then(count => {
+          this.featuredButtonLabel = this.orderServ.nameProductLine + ' (' + count + ')';
         })
-     
+
       }
-        
+
 
     });
   }
@@ -146,27 +149,28 @@ export class ProductosTabStructureListComponent implements OnInit, OnDestroy {
       this.tpsSeleccionada,
       this.psSeleccionada,
     )
-      this.productStructureService.getLowestsProductStructuresByCoProductStructuresAndIdEnterprise(this.dbServ.getDatabase(),this.psSeleccionada.coProductStructure, this.psSeleccionada.idEnterprise).then(() => {
-        this.productStructures = false;
-        this.productService.onProductStructureCLicked();
-      });
-
+    this.productStructureService.getLowestsProductStructuresByCoProductStructuresAndIdEnterprise(this.dbServ.getDatabase(), this.psSeleccionada.coProductStructure, this.psSeleccionada.idEnterprise).then(() => {
+      this.productStructures = false;
+      this.productService.onProductStructureCLicked();
+      this.cdr.detectChanges();
+    });
+    this.cdr.detectChanges();
 
   }
 
-  onFeaturedProductSelected(){
+  onFeaturedProductSelected() {
     this.productStructures = false;
     this.productStructureService.nombreProductStructureSeleccionada = this.featuredButtonLabel.split('(')[0];;
     this.productService.onFeaturedStructureClicked();
   }
 
-  onFavoriteProductSelected(){
+  onFavoriteProductSelected() {
     this.productStructures = false;
     this.productStructureService.nombreProductStructureSeleccionada = this.favoriteButtonLabel.split('(')[0];
     this.productService.onFavoriteStructureClicked();
   }
 
-  onCartSelected(){
+  onCartSelected() {
     this.productStructures = false;
     this.productService.onCarritoButtonClicked();
   }
