60cb93d80c15d4d2aff5e5b2ad85d092805b95d8
KibernoDiegoBranco <diego.branco@kiberno.com>
2025-11-11 11:54:20 -0400

    [CurrencyService] Booleanos salian como strings, lo que jode todo.

--- DenarioPremiunMovil/src/app/services/currency/currency.service.ts
+++ DenarioPremiunMovil/src/app/services/currency/currency.service.ts
@@ -95,6 +95,7 @@ export class CurrencyService {
     if (cm) {
       return cm;
     } else {
+      console.warn("[CurrencyService] No se encontró el módulo de moneda: " + coModule);
       return new CurrencyModules(0, 0, true, false);
     }
   }
@@ -346,10 +347,10 @@ getCurrencyModules(db: SQLiteObject) {
       for (let i = 0; i < data.rows.length; i++) {
         var item = data.rows.item(i);
         var cm: CurrencyModules = {
-          idCurrencyModule: item.id_currency_module,
+          idCurrencyModules: item.id_currency_modules,
           idModule: item.id_module,
-          localCurrencyDefault: item.local_currency_default,
-          showConversion: item.show_conversion,
+          localCurrencyDefault: item.local_currency_default.toLowerCase() === 'true',
+          showConversion: item.show_conversion.toLowerCase() === 'true',
         };
         map.set(item.co_module, cm);
       }
