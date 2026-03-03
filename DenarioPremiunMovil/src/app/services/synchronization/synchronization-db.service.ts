806b2bc8b70cc3ee1744ca24e17b94ddf14ec1fa
KibernoDiegoBranco <diego.branco@kiberno.com>
2026-03-03 11:33:04 -0400

    Nuevo script: agrega columna a return_types

--- DenarioPremiunMovil/src/app/services/synchronization/synchronization-db.service.ts
+++ DenarioPremiunMovil/src/app/services/synchronization/synchronization-db.service.ts
@@ -95,7 +95,7 @@ export class SynchronizationDBService {
   private tables: any[] = [];
   public tablaSincronizando: string = "";
   public inHome: Boolean = true;
-  private CURRENT_DB_VERSION: number = 4;
+  private CURRENT_DB_VERSION: number = 5;
 
   constructor(
     private navController: NavController,
