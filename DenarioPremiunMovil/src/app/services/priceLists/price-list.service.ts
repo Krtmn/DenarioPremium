23f96304289bc0b3155d32dd7abe9e8c53ba06c1
KibernoDiegoBranco <diego.branco@kiberno.com>
2026-04-29 16:35:16 -0400

    [Productos] Arreglada conversion errada de precios en detalle de producto al cambiar lista de precio

--- DenarioPremiunMovil/src/app/services/priceLists/price-list.service.ts
+++ DenarioPremiunMovil/src/app/services/priceLists/price-list.service.ts
@@ -74,13 +74,12 @@ export class PriceListService {
           idProductQuery,
           data.rows.item(0).nu_price_default, // Precio de la lista de precio
           coCurrency, // moneda de la lista de precio
-          data.rows.item(0).co_currency === this.currencyService.getLocalCurrency ?
+          this.currencyService.isLocalCurrency(coCurrency) ?
             this.currencyService.toHardCurrency(data.rows.item(0).nu_price_default) :
             this.currencyService.toLocalCurrency(data.rows.item(0).nu_price_default), // Precio en la moneda opuesta a la lista de precio
-          data.rows.item(0).co_currency === this.currencyService.getLocalCurrency ?
-            this.currencyService.hardCurrency.coCurrency :
-            this.currencyService.localCurrency.coCurrency, // moneda opuesta a la lista de precio
+          this.currencyService.oppositeCoCurrency(coCurrency), // moneda opuesta a la lista de precio
         );
+        console.log('productPrice: ' + JSON.stringify(this.productPrice));
       }).catch(e => {
         console.log("[PriceListService] Error al cargar getPriceListByIdListAndIdProduct.");
         console.log(e);
