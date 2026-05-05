6154f627fd524c4f227caa37ea483d0ce3a98614
KibernoDiegoBranco <diego.branco@kiberno.com>
2026-05-05 10:46:14 -0400

    Subido version de DB para que use script

--- DenarioPremiunMovil/src/app/services/synchronization/synchronization-db.service.ts
+++ DenarioPremiunMovil/src/app/services/synchronization/synchronization-db.service.ts
@@ -113,7 +113,7 @@ export class SynchronizationDBService {
   private tables: any[] = [];
   public tablaSincronizando: string = "";
   public inHome: Boolean = true;
-  private CURRENT_DB_VERSION: number = 7;
+  private CURRENT_DB_VERSION: number = 8;
 
   constructor(
     private navController: NavController,
