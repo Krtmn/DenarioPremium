8dc4ccde7322fc3f52d9494777692e5be15343ce
luisCastillo966 <luis.castillo@kiberno.com>
2026-08-19 19:05:15 -0400

    feat(pedidos): se añade manual de pruebas y catálogo de tests unitarios para mejorar la validación de pedidos

--- DenarioPremiunMovil/src/app/pedidos/pedido/pedido.component.spec.ts
+++ DenarioPremiunMovil/src/app/pedidos/pedido/pedido.component.spec.ts
@@ -39,6 +39,10 @@ describe('PedidoComponent', () => {
       'getTag',
       'setup',
       'productSummary',
+      'resetOrderValidationUxFlags',
+      'resetOrderExitBaseline',
+      'updateSaveButtonAvailability',
+      'updateSendButtonAvailability',
     ]);
     Object.assign(orderServMock, {
       openOrder: false,
@@ -51,6 +55,10 @@ describe('PedidoComponent', () => {
       pedidoModificable: true,
       ProdSelecttags: new Map<string, string>(),
       signatureOrder: false,
+      disableSaveButton: true,
+      disableSendButton: true,
+      orderValidToSave: new Subject<boolean>(),
+      orderValidToSend: new Subject<boolean>(),
     });
     orderServMock.getTag.and.returnValue('');
     orderServMock.setup.and.resolveTo(undefined);
--- DenarioPremiunMovil/src/app/pedidos/pedidos.component.spec.ts
+++ DenarioPremiunMovil/src/app/pedidos/pedidos.component.spec.ts
@@ -1,7 +1,16 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { IonicModule, Platform } from '@ionic/angular';
+import { Subject } from 'rxjs';
 
 import { PedidosComponent } from './pedidos.component';
+import { PedidosService } from './pedidos.service';
+import { MessageService } from 'src/app/services/messageService/message.service';
+import { GeolocationService } from '../services/geolocation/geolocation.service';
+import { EnterpriseService } from '../services/enterprise/enterprise.service';
+import { SynchronizationDBService } from '../services/synchronization/synchronization-db.service';
+import { ProductStructureService } from '../services/productStructures/product-structure.service';
+import { DateServiceService } from '../services/dates/date-service.service';
 
 describe('PedidosComponent', () => {
   let component: PedidosComponent;
@@ -10,8 +19,49 @@ describe('PedidosComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PedidosComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: PedidosService,
+          useValue: {
+            coordenadas: '',
+            userMustActivateGPS: false,
+            getTags: () => undefined,
+            getLists: () => undefined,
+            getConfiguration: () => undefined,
+          },
+        },
+        { provide: Router, useValue: { navigate: jasmine.createSpy('navigate') } },
+        { provide: MessageService, useValue: { showLoading: () => Promise.resolve() } },
+        { provide: GeolocationService, useValue: { getCurrentPosition: () => Promise.resolve('') } },
+        { provide: DateServiceService, useValue: {} },
+        {
+          provide: EnterpriseService,
+          useValue: {
+            setup: () => Promise.resolve(),
+            defaultEnterprise: () => ({ idEnterprise: 1 }),
+          },
+        },
+        {
+          provide: SynchronizationDBService,
+          useValue: { getDatabase: () => ({}) },
+        },
+        {
+          provide: ProductStructureService,
+          useValue: {},
+        },
+        {
+          provide: Platform,
+          useValue: {
+            backButton: {
+              subscribeWithPriority: () => new Subject<void>().subscribe(),
+            },
+          },
+        },
+      ],
+    })
+      .overrideComponent(PedidosComponent, { set: { template: '' } })
+      .compileComponents();
 
     fixture = TestBed.createComponent(PedidosComponent);
     component = fixture.componentInstance;
