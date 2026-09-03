3d7582249bcdc5df4b297a41c7cdff660f73f860
KibernoDiegoBranco <diego.branco@kiberno.com>
2026-09-03 09:36:33 -0400

    Nueva Version Especial 6.6.21.5

--- DenarioPremiunMovil/src/app/login/login.component.ts
+++ DenarioPremiunMovil/src/app/login/login.component.ts
@@ -63,7 +63,9 @@ export class LoginComponent implements OnInit {
   public isIOS: boolean = false; // <-- NUEVA PROPIEDAD
   public showFooter: boolean = true; // <-- NUEVA PROPIEDAD
   public currentYear: number = new Date().getFullYear();
-  private static readonly FALLBACK_VERSION = '6.6.21.4';
+
+  private static readonly FALLBACK_VERSION = '6.6.21.5';
+
 
 
   async ngOnInit() {
